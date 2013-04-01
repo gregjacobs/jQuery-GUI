28cf4beb95946cdfbd813940dd0c628d6f41a146
Gregory Jacobs <greg@greg-jacobs.com>
2013-03-31 21:47:41 -0400

    Shorten implementation of Css.mapToString() using Lo-Dash.

--- js/util/Css.js
+++ js/util/Css.js
@@ -1,9 +1,10 @@
 /*global define */
 define( [
 	'jquery',
+	'lodash',
 	'ui/UI'
 ],
-function( jQuery, UI ) {
+function( jQuery, _, UI ) {
 	
 	/**
 	 * @class UI.Css
@@ -33,11 +34,11 @@ function( jQuery, UI ) {
 		
 		
 		/**
-		 * Given a hash of CSS property/value pairs, will return a string that can be placed directly
+		 * Given an Object (map) of CSS property/value pairs, will return a string that can be placed directly
 		 * into the "style" attribute of an element. camelCased CSS property names will be converted to 
 		 * dashes. Ex: "fontSize" will be converted to "font-size".
 		 * 
-		 * @param {Object} cssProperties An object (hash) of CSS property/value pairs. Ex: { color: 'red', fontSize: '10px;' }
+		 * @param {Object} cssProperties An Object (map) of CSS property/value pairs. Ex: { color: 'red', fontSize: '10px;' }
 		 * @return {String} The CSS string that can be used directly in an element's "style" attribute, or when using it
 		 *   to update an existing element's styles, can be used directly on the .style.cssText property.
 		 */
@@ -45,13 +46,11 @@ function( jQuery, UI ) {
 			var replaceRegex = /([A-Z])/g,
 			    cssString = "",
 			    normalizedProp;
-			    
-			for( var prop in cssProperties ) {
-				if( cssProperties.hasOwnProperty( prop ) ) {
-					normalizedProp = prop.replace( replaceRegex, '-$1' ).toLowerCase();
-					cssString += normalizedProp + ':' + cssProperties[ prop ] + ';';
-				}
-			}
+			
+			_.forOwn( cssProperties, function( value, prop ) {
+				normalizedProp = prop.replace( replaceRegex, '-$1' ).toLowerCase();
+				cssString += normalizedProp + ':' + value + ';';
+			} );
 			return cssString;
 		},
 		
