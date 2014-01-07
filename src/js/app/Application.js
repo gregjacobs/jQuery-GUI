be57039a2147ff6062f2a555b119bc21fa55cb70
Gregory Jacobs <gregory.jacobs@credit-suisse.com>
2014-01-07 17:04:42 -0500

    Change `createControllers()` in Application to return an Object (map) of the controllers being created for the Application. Controllers are now accessed by name via `getController()` after they have been instantiated.
    Also add destroy() functionality for the application, which destroys the controllers, and the viewport when called.

--- src/js/app/Application.js
+++ src/js/app/Application.js
@@ -26,9 +26,9 @@ define( [
 	 * This class implements the initial set up for an Application as the page loads. It instantiates any data containers
 	 * ({@link data.Model Models} or {@link data.Collection Collections}) that the app may need from the 
 	 * {@link #createDataContainers} method, a {@link gui.Viewport Viewport} from the {@link #createViewport} method, and 
-	 * one or more  {@link gui.app.Controller Controllers} from the {@link #createControllers} method, which will manage 
-	 * interactions with the components in the Viewport. When the page (document) is ready, the {@link #onDocumentReady} 
-	 * method is called to automatically render the Viewport at the appropriate time.
+	 * one or more {@link gui.app.Controller Controllers} from the {@link #createControllers} method, which will manage 
+	 * interactions with the data containers and components in the Viewport. When the page (document) is ready, the 
+	 * {@link #onDocumentReady} method is called to automatically render the Viewport at the appropriate time.
 	 * 
 	 * Subclasses must implement the {@link #createViewport} method. They may also override the {@link #createDataContainers}, 
 	 * {@link #createControllers}, and the {@link #init} method for any subclass-specific initialization. The {@link #init} 
@@ -71,11 +71,13 @@ define( [
 	 *             },
 	 *             
 	 *             createControllers : function( viewport ) {
-	 *                 this.filtersController = new FiltersController( { view: viewport } );
-	 *                 
-	 *                 this.mainDisplayController = new MainDisplayController( {
-	 *                     view: viewport, 
-	 *                     usersCollection: this.usersCollection  // so the MainDisplayController can re-load this collection when a filter changes
+	 *                 return {
+	 *                     filters : new FiltersController( { view: viewport } ),
+	 *                     
+	 *                     mainDisplay : new MainDisplayController( {
+	 *                         view: viewport, 
+	 *                         usersCollection: this.usersCollection  // so the MainDisplayController can re-load this collection when a filter changes
+	 *                     } )
 	 *                 } );
 	 *             },
 	 *             
@@ -83,13 +85,13 @@ define( [
 	 *             init : function() {
 	 *                 this._super( arguments );
 	 *                 
-	 *                 this.filtersController.on( 'filterselect', this.onFilterSelect, this );
+	 *                 this.getController( 'filters' ).on( 'filterselect', this.onFilterSelect, this );
 	 *             },
 	 *             
 	 *             
 	 *             onFilterSelect : function() {
-	 *                 var selectedFilters = this.filtersController.getSelectedFilters();
-	 *                 this.mainDisplayController.update( selectedFilters );
+	 *                 var selectedFilters = this.getController( 'filters' ).getSelectedFilters();
+	 *                 this.getController( 'mainDisplay' ).update( selectedFilters );
 	 *             }
 	 *             
 	 *         } );
@@ -125,19 +127,61 @@ define( [
 		abstractClass: true,
 		
 		/**
+		 * @cfg {String/HTMLElement/jQuery} renderTo
+		 * 
+		 * A selector, HTMLElement, or jQuery wrapped set of where to render the {@link #viewport} when 
+		 * the document is ready.
+		 * 
+		 * By default, the {@link #viewport} is rendered to the document body, but it may be useful to render
+		 * an entire application into say, a {@link gui.window.Window}.
+		 */
+		renderTo : 'body',  // renders to the document body
+		
+		
+		/**
 		 * @protected
 		 * @property {gui.Viewport} viewport
 		 * 
 		 * The viewport that was instantiated for the Application via {@link #createViewport}.
 		 */
 		
+		/**
+		 * @private
+		 * @property {Object} controllers
+		 * 
+		 * An Object (map) of the {@link gui.app.Controller Controllers} instantiated for the Application
+		 * from {@link #createControllers}. The keys are the controller names, and the values are the 
+		 * {@link gui.app.Controller} instances.
+		 * 
+		 * Individual controllers should be referenced by name via {@link #getController}.
+		 */
+		
+		/**
+		 * @private
+		 * @property {Boolean} destroyed
+		 * 
+		 * Flag which is set to `true` once the Application has been destroyed.
+		 */
+		
 		
 		/**
 		 * @constructor
+		 * @param {Object} [cfg] The configuration options for this class, specified in an Object (map).
 		 */
 		constructor : function( cfg ) {
-			_.assign( this, cfg );  // assign the configuration options onto this object
+			_.assign( this, cfg );     // assign the configuration options onto this object
+			this._super( arguments );  // call the superclass constructor (Observable)
 
+			this.addEvents(
+				/**
+				 * Fires when the Application has been {@link #method-destroy destroyed}.
+				 * 
+				 * @event destroy
+				 * @param {gui.app.Application} application This Application instance.
+				 */
+				'destroy'
+			);
+			
 			this.createDataContainers();
 			
 			var viewport = this.viewport = this.createViewport();
@@ -145,7 +189,10 @@ define( [
 			if( !viewport ) throw new Error( "Error: No viewport returned by createViewport() method" );
 			// </debug>
 			
-			this.createControllers( viewport );
+			var controllers = this.controllers = this.createControllers( viewport );
+			// <debug>
+			if( !controllers ) throw new Error( "Error: No Object (map) returned by the createControllers() method" );
+			// </debug>
 			
 			this.init();
 			
@@ -173,10 +220,12 @@ define( [
 		 *     }
 		 *     
 		 *     createControllers : function( viewport ) {
-		 *         this.usersController = new UsersController( {
-		 *             view : viewport,
-		 *             usersCollection : this.usersCollection  // so this collection can be managed by the UsersController
-		 *         } );
+		 *         return {
+		 *             users: new UsersController( {
+		 *                 view : viewport,
+		 *                 usersCollection : this.usersCollection  // so this collection can be managed by the UsersController
+		 *             } )
+		 *         };
 		 *     }
 		 * 
 		 * @protected
@@ -195,8 +244,16 @@ define( [
 		
 		
 		/**
-		 * Method which may be overridden by subclasses to instantiate one or more {@link gui.app.Controller Controllers}. 
-		 * References to these controllers may be saved to this Application object from this method.
+		 * Method which may be overridden by subclasses to instantiate and return one or more 
+		 * {@link gui.app.Controller Controllers}. 
+		 * 
+		 * The return value from this method is an Object (map) where the keys are the names to reference the controller(s) by, 
+		 * and the values are the {@link gui.app.Controller} instances. Controllers are then later referenced via
+		 * {@link #getController}. 
+		 * 
+		 * The reason that controllers are returned from this method instead of simply creating direct references to them is 
+		 * so that the Application instance can manage them, and destroy them if the Application itself is 
+		 * {@link #method-destroy destroyed}.
 		 * 
 		 * ## Examples
 		 * 
@@ -205,21 +262,68 @@ define( [
 		 * Single controller:
 		 * 
 		 *     createControllers : function( viewport ) {
-		 *         this.appController = new Controller( { view: viewport } );  // save a named reference for later
+		 *         return {
+		 *             appController: new AppController( { view: viewport } )
+		 *         };
 		 *     }
 		 *     
 		 * Multiple controllers:
 		 * 
 		 *     createControllers : function( viewport ) {
-		 *         this.filtersController = new Controller( { view: viewport } );
-		 *         this.displayController = new Controller( { view: viewport } );
+		 *         return {
+		 *             filters: new FiltersController( { view: viewport } ),
+		 *             display: new DisplayController( { view: viewport } )
+		 *         };
+		 *     }
+		 *     
+		 * You may also choose to implement factory methods for each controller, either for use with unit tests (where
+		 * mock controllers can be substituted), or for subclasses to return different Controller types. For example:
+		 * 
+		 *     createControllers : function( viewport ) {
+		 *         return {
+		 *             filters: this.createFiltersController( viewport ),
+		 *             display: this.createDisplayController( viewport )
+		 *         };
+		 *     },
+		 *     
+		 *     createFiltersController : function( viewport ) {
+		 *         return new FiltersController( { view: viewport } );
+		 *     },
+		 *     
+		 *     createDisplayController : function( viewport ) {
+		 *         return new DisplayController( { view: viewport } );
+		 *     }
+		 * 
+		 * And finally, when subclassing `createControllers`, one may add controllers to the return map in the following
+		 * fashion:
+		 * 
+		 *     // in superclass
+		 *     createControllers : function( viewport ) {
+		 *         return {
+		 *             filters: new FiltersController( { view: viewport } )
+		 *         };
+		 *     }
+		 * 
+		 * 
+		 *     // in subclass
+		 *     createControllers : function( viewport ) {
+		 *         var controllers = this._super( arguments );
+		 *         
+		 *         // Add controller(s) for this subclass
+		 *         controllers.display = new DisplayController( { view: viewport } );
+		 *         
+		 *         return controllers;
 		 *     }
 		 * 
 		 * @protected
 		 * @param {gui.Viewport} viewport A reference to the Viewport created from {@link #createViewport}. This is
 		 *   usually passed to a Controller as the {@link gui.app.Controller#view view} config.
+		 * @return {Object} An Object (map) of the controllers, where the key names are the controller names, and the
+		 *   values are {@link gui.app.Controller} instances.
 		 */
-		createControllers : _.noop,
+		createControllers : function() {
+			return {};  // empty map by default
+		},
 		
 		
 		/**
@@ -236,7 +340,7 @@ define( [
 		 *         
 		 *         this.viewport.on( 'render', function() { ... } );  // assuming we want to listen for this event (probably don't need to)
 		 *         
-		 *         this.myController.doSomething();  // `myController` would have been a reference set up in {@link #createControllers}
+		 *         this.getController( 'myController').doSomething();  // `myController` would have been a controller set up in {@link #createControllers}
 		 *     }
 		 * 
 		 * @protected
@@ -251,7 +355,97 @@ define( [
 		 * @protected
 		 */
 		onDocumentReady : function() {
-			this.viewport.render( 'body' );
+			this.viewport.render( this.renderTo );
+		},
+		
+		
+		// ------------------------------------
+		
+		
+		/**
+		 * Retrieves a controller by name. Controllers are set up from the {@link #createControllers} method.
+		 * 
+		 * Example:
+		 * 
+		 *     // set up a controller in this method
+		 *     createControllers : function() {
+		 *         return {
+		 *             mainDisplay: new MainDisplayController()
+		 *         };
+		 *     },
+		 *     
+		 *     ...
+		 *     
+		 *     // Retrieve the controller from another method
+		 *     init : function() {
+		 *         this._super( arguments );
+		 *         
+		 *         var mainDisplayController = this.getController( 'mainDisplay' );
+		 *         mainDisplayController.doSomething();
+		 *     }
+		 * 
+		 * @protected
+		 * @param {String} `name`
+		 * @return {gui.app.Controller} The controller for the given `name`, or `null` if there is no controller
+		 *   registered for that name.
+		 */
+		getController : function( name ) {
+			return this.controllers[ name ] || null;
+		},
+		
+		
+		// ------------------------------------
+		
+		// Destruction Functionality
+		
+		
+		/**
+		 * Destroys the Application.
+		 * 
+		 * Note: Subclasses should override the {@link #onDestroy} hook method instead of this method for their
+		 * own subclass-specific destruction logic.
+		 */
+		destroy : function() {
+			if( !this.isDestroyed() ) {
+				this.onDestroy();  // call hook method
+				
+				// Destroy controllers first
+				_.forOwn( this.controllers, function( controller ) { controller.destroy(); } );
+				
+				// Destroy the viewport second
+				this.viewport.destroy();
+				
+				this.destroyed = true;
+				this.fireEvent( 'destroy', this );  // note: fire 'destroy' event before purging listeners!
+				this.purgeListeners();
+			}
+		},
+		
+		
+		/**
+		 * Hook method for the destruction of the Application.
+		 * 
+		 * Subclasses should call their superclass method after any subclass-specific processing. For example:
+		 * 
+		 *     onDestroy : function() {
+		 *         this.popupWindow.destroy();  // destroy a {@link gui.window.Window} that was created by the subclass
+		 *         
+		 *         this._super( arguments );    // now call the superclass method
+		 *     }
+		 * 
+		 * @protected
+		 * @template
+		 */
+		onDestroy : _.noop,
+		
+		
+		/**
+		 * Determines if the Application has been destroyed.
+		 * 
+		 * @return {Boolean}
+		 */
+		isDestroyed : function() {
+			return !!this.destroyed;
 		}
 		
 	} );
--- tests/spec/app/ApplicationSpec.js
+++ tests/spec/app/ApplicationSpec.js
@@ -1,33 +1,216 @@
 /*global define, describe, beforeEach, afterEach, it, expect, spyOn */
 define( [
+	'lodash',
+	
 	'gui/app/Application',
 	'gui/app/Controller',
 	'gui/Viewport'
-], function( Application, Controller, Viewport ) {
+], function( _, Application, Controller, Viewport ) {
 	
 	describe( 'gui.app.Application', function() {
+		var viewport,
+		    ConcreteApplication;  // a concrete subclass for testing
+		
+		
+		beforeEach( function() {
+			viewport = new Viewport();
+			
+			ConcreteApplication = Application.extend( {
+				createViewport : function() { return viewport; }
+			} );
+		} );
+		
 		
 		describe( 'constructor', function() {
 			
-			it( "should throw an error if a Viewport is not returned from createViewport()", function() {
-				var MyApplication = Application.extend( {
-					createViewport : function() {}  // note: no return value
+			describe( "`listeners` config", function() {
+				
+				it( "should be able to accept the `listeners` config for Observable (the superclass)", function() {
+					var testEventCallCount = 0;
+					
+					var app = new ConcreteApplication( {
+						listeners : {
+							'testEvent' : function() { testEventCallCount++; }
+						}
+					} );
+					
+					expect( testEventCallCount ).toBe( 0 );  // initial condition
+					
+					app.fireEvent( 'testEvent' );  // manually firing an event just for testing
+					expect( testEventCallCount ).toBe( 1 );
 				} );
 				
-				expect( function() {
-					var app = new MyApplication();
-				} ).toThrow( "Error: No viewport returned by createViewport() method" );
 			} );
 			
 			
-			it( "should store the Viewport which is returned from createViewport() as the `viewport` property", function() {
-				var viewport = new Viewport();
-				var MyApplication = Application.extend( {
-					createViewport : function() { return viewport; }
+			describe( "viewport functionality", function() {
+			
+				it( "should throw an error if a Viewport is not returned from createViewport()", function() {
+					var MyApplication = Application.extend( {
+						createViewport : function() {}  // note: no return value
+					} );
+					
+					expect( function() {
+						var app = new MyApplication();
+					} ).toThrow( "Error: No viewport returned by createViewport() method" );
 				} );
 				
-				var app = new MyApplication();
-				expect( app.viewport ).toBe( viewport );
+				
+				it( "should store the Viewport which is returned from createViewport() as the `viewport` property", function() {
+					var viewport = new Viewport();
+					var MyApplication = Application.extend( {
+						createViewport : function() { return viewport; }
+					} );
+					
+					var app = new MyApplication();
+					expect( app.viewport ).toBe( viewport );
+				} );
+				
+			} );
+			
+			
+			describe( "controller functionality", function() {
+				
+				it( "should provide the `viewport` instance to the createControllers() method", function() {
+					var viewport = new Viewport(),
+					    argProvidedViewport;
+					
+					var MyApplication = Application.extend( {
+						createViewport : function() { return viewport; },
+						createControllers : function( viewport ) {
+							argProvidedViewport = viewport;
+							
+							return this._super( arguments );
+						}
+					} );
+					
+					var application = new MyApplication();
+					expect( argProvidedViewport ).toBe( viewport );
+				} );
+				
+
+				it( "should throw an error if no Object (map) is returned from the createControllers() method", function() {
+					var TestApplication = ConcreteApplication.extend( {
+						createControllers : function() {}  // note: no return Object (map)
+					} );
+					
+					var application;
+					expect( function() {
+						application = new TestApplication();
+					} ).toThrow( "Error: No Object (map) returned by the createControllers() method" );
+				} );
+				
+				
+				it( "should not error if an empty Object (map) is returned from the createControllers() method", function() {
+					var TestApplication = ConcreteApplication.extend( {
+						createControllers : function() {
+							return {};
+						}
+					} );
+					
+					var application;
+					expect( function() {
+						application = new TestApplication();
+					} ).not.toThrow();
+					
+					expect( application.getController( 'nonExistent' ) ).toBe( null );
+				} );
+				
+				
+				it( "should store controllers returned in the map, which should be accessible via getController()", function() {
+					var controller1 = new Controller( { view: viewport } ),
+					    controller2 = new Controller( { view: viewport } );
+					
+					var TestApplication = ConcreteApplication.extend( {
+						createControllers : function() {
+							return {
+								controller1: controller1,
+								controller2: controller2
+							};
+						}
+					} );
+					
+					var application = new TestApplication();
+
+					expect( application.getController( 'controller1' ) ).toBe( controller1 );
+					expect( application.getController( 'controller2' ) ).toBe( controller2 );
+					expect( application.getController( 'controller3' ) ).toBe( null );
+				} );
+				
+			} );
+			
+		} );
+		
+		
+		describe( 'destroy', function() {
+			var viewport,
+			    controller1,
+			    controller2,
+			    application;
+			
+			beforeEach( function() {
+				viewport = new Viewport();
+				controller1 = new Controller( { view: viewport } );
+				controller2 = new Controller( { view: viewport } );
+				
+				var MyApplication = Application.extend( {
+					createViewport : function() { 
+						return viewport; 
+					},
+					
+					createControllers : function( viewport ) {
+						return {
+							controller1: controller1,
+							controller2: controller2
+						};
+					}
+				} );
+				application = new MyApplication();
+			} );
+			
+			
+			it( "should fire the 'destroy' event", function() {
+				var destroyCallCount = 0;
+				
+				application.on( 'destroy', function( app ) {
+					destroyCallCount++;
+					
+					expect( app ).toBe( application );  // make sure the event is called with the correct argument
+				} );
+				
+				application.destroy();
+				expect( destroyCallCount ).toBe( 1 );
+			} );
+			
+			
+			it( "should destroy the Viewport", function() {
+				expect( viewport.isDestroyed() ).toBe( false );  // initial condition
+				
+				application.destroy();
+				expect( viewport.isDestroyed() ).toBe( true );
+			} );
+			
+			
+			it( "should destroy each of the Controllers", function() {
+				expect( controller1.isDestroyed() ).toBe( false );  // initial condition
+				expect( controller2.isDestroyed() ).toBe( false );  // initial condition
+				
+				application.destroy();
+				expect( controller1.isDestroyed() ).toBe( true );
+				expect( controller2.isDestroyed() ).toBe( true );
+			} );
+			
+			
+			it( "should only be able to be destroyed once", function() {
+				spyOn( application, 'onDestroy' ).andCallThrough();      // testing via the onDestroy() hook method
+				
+				expect( application.onDestroy.calls.length ).toBe( 0 );  // initial condition
+
+				application.destroy();
+				expect( application.onDestroy.calls.length ).toBe( 1 );
+				
+				application.destroy();
+				expect( application.onDestroy.calls.length ).toBe( 1 );  // should still be 1 (i.e. shouldn't have been called again)
 			} );
 			
 		} );
